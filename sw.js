71698aab0d97c6c0c39b1d215d11b93092aee202
Morgan Sziraki <morgan.sziraki@gmail.com>
2026-04-30 00:31:05 +0100

    fix sw

--- sw.js
+++ sw.js
@@ -1,18 +1,3 @@
-const CACHE = "blog-cache-v1";
-const OFFLINE_URLS = [
-  "/",
-  "/index.html",
-  "/assets/css/site.css"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(OFFLINE_URLS))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
+self.addEventListener("fetch", () => {});
